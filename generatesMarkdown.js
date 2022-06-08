3a9b1a4d9e71e84b2da22f2b877d0b13a165a554
NumiBlue <thekindlyblog@gmail.com>
2022-06-08 06:59:10 -0400

    adds js to generate markdown instead of portfolio

---
+++ generatesMarkdown.js
@@ -0,0 +1,33 @@
+
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+    return `# ${data.title}
+  
+    ## Table of Contents
+  [Description](#description)
+  [Installation](#installation)
+  [Usage](Usage)
+  [Contribution](Contribution)
+  [Testing](Testing)
+  [License](License)
+  [Questions](Questions)
+  ## Description
+  ${data.description}
+  ## Installation
+  ${data.installation}
+  ## Usage
+  ${data.usage}
+  ## Contribution
+  ${data.contribution}
+  ## Testing
+  ${data.testing}
+  ## License
+  ${data.license}
+  ## Questions
+  ${data.github}
+  `;
+  }
+  
+  module.exports = generateMarkdown;
+  
